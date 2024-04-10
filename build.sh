5e6af15d3e56b240aaf958f49f9dfb9014406ca2
= <mahlstromerv@gmail.com>
2024-04-10 16:12:47 -0700

    Cross platform with linux and windows

--- build.sh
+++ build.sh
@@ -6,39 +6,43 @@ appName="UntitledPlatformer"
 if [[ $config = "run" ]]
 then
     mode="$2"
+    system="$3"
+
     if [[ $mode = "debug" ]]
     then
-        cd "bin/Debug-linux/$appName"
+        cd "bin/Debug-$system/$appName"
         ./$appName
     elif [[ $mode = "release" ]]
     then
-        cd "bin/Release-linux/$appName"
+        cd "bin/Release-$system/$appName"
         ./$appName
     elif [[ $mode = "dist" ]]
     then
-        cd "bin/Dist-linux/$appName"
+        cd "bin/Dist-$system/$appName"
         ./$appName
     fi
 elif [[ $config = "assets" ]]
 then
     mode="$2"
+    system="$3"
     if [[ $mode = "debug" ]]
     then
-        cp -r App/assets/ bin/Debug-linux/Application/
+        cp -r "App/assets/" "bin/Debug-$system/Application/"
     elif [[ $mode = "release" ]]
     then
-        cp -r App/assets/ bin/Release-linux/Application/
+        cp -r "App/assets/" "bin/Release-$system/Application/"
     elif [[ $mode = "dist" ]]
     then
-        cp -r App/assets/ bin/Dist-linux/Application/
+        cp -r "App/assets/" "bin/Dist-$system/Application/"
     fi
 
 elif [[ $config = "clean" ]]
 then
     rm -rf bin build
 else
-    vendor/premake/linux/premake5 export-compile-commands
-    vendor/premake/linux/premake5 gmake
+    system="$2"
+    ./"vendor/premake/$system/premake5" export-compile-commands
+    ./"vendor/premake/$system/premake5" gmake
     make all config=$config -j4
     cp compile_commands/debug.json compile_commands.json
 fi
